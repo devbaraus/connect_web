16fdb77232b09c304ceb7f5d8a55d5b16dbd6092
Bruno de Araújo Alves <b.araujo@kingspanisoeste.com.br>
2024-03-12 12:47:14 -0300

    refactor: ♻️ remove api route for now

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,14 +1,14 @@
-import { drizzle } from 'drizzle-orm/postgres-js';
-import postgres from 'postgres';
-import { DATABASE_URL } from '$env/static/private';
-import * as schema from './migrations/schema';
-import * as relations from './migrations/schema_relations';
-
-// for query purposes
-const pgClient = postgres(DATABASE_URL);
-export const db = drizzle(pgClient, {
-	schema: {
-		...schema,
-		...relations
-	}
-});
+// import { drizzle } from 'drizzle-orm/postgres-js';
+// import postgres from 'postgres';
+// import { DATABASE_URL } from '$env/static/private';
+// import * as schema from './migrations/schema';
+// import * as relations from './migrations/schema_relations';
+//
+// // for query purposes
+// const pgClient = postgres(DATABASE_URL);
+// export const db = drizzle(pgClient, {
+// 	schema: {
+// 		...schema,
+// 		...relations
+// 	}
+// });
