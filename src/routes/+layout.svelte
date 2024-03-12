25bb2f7365269a945fab69e156fd0eb25fbcd4f8
Bruno de Araújo Alves <b.araujo@kingspanisoeste.com.br>
2024-03-12 12:39:37 -0300

    feat: ✨ add tailus template

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,7 @@
-<script>import "../app.pcss";</script><slot></slot>
+<script>import "../app.css";
+import Header from "$lib/components/layout/header.svelte";
+import Footer from "$lib/components/layout/footer.svelte";
+</script>
+<Header/>
+<slot></slot>
+<Footer/>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,64 +1,51 @@
-import { fontFamily } from "tailwindcss/defaultTheme";
+import themer from "@tailus/themer";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 const config = {
-	darkMode: ["class"],
+	darkMode: "media",
 	content: ["./src/**/*.{html,js,svelte,ts}"],
-  safelist: ["dark"],
+	safelist: ["isToggled"],
 	theme: {
-		container: {
-			center: true,
-			padding: "2rem",
-			screens: {
-				"2xl": "1400px"
-			}
+		colors: ({ colors }) => ({
+			primary: colors.blue,
+			secondary: colors.lime,
+			accent: colors.pink,
+			success: colors.lime,
+			danger: colors.red,
+			warning: colors.yellow,
+			info: colors.blue,
+			gray: colors.zinc,
+			white: colors.white,
+			black: colors.black,
+			transparent: colors.transparent,
+		}),
+		fontFamily: {
+			sans: ['Geist', 'Inter', ...defaultTheme.fontFamily.sans],
+			mono : ['GeistMono', 'fira-code', ...defaultTheme.fontFamily.mono],
 		},
-		extend: {
-			colors: {
-				border: "hsl(var(--border) / <alpha-value>)",
-				input: "hsl(var(--input) / <alpha-value>)",
-				ring: "hsl(var(--ring) / <alpha-value>)",
-				background: "hsl(var(--background) / <alpha-value>)",
-				foreground: "hsl(var(--foreground) / <alpha-value>)",
-				primary: {
-					DEFAULT: "hsl(var(--primary) / <alpha-value>)",
-					foreground: "hsl(var(--primary-foreground) / <alpha-value>)"
+		keyframes: {
+			loop: {
+				to: {
+					"offset-distance": "100%",
 				},
-				secondary: {
-					DEFAULT: "hsl(var(--secondary) / <alpha-value>)",
-					foreground: "hsl(var(--secondary-foreground) / <alpha-value>)"
-				},
-				destructive: {
-					DEFAULT: "hsl(var(--destructive) / <alpha-value>)",
-					foreground: "hsl(var(--destructive-foreground) / <alpha-value>)"
-				},
-				muted: {
-					DEFAULT: "hsl(var(--muted) / <alpha-value>)",
-					foreground: "hsl(var(--muted-foreground) / <alpha-value>)"
-				},
-				accent: {
-					DEFAULT: "hsl(var(--accent) / <alpha-value>)",
-					foreground: "hsl(var(--accent-foreground) / <alpha-value>)"
-				},
-				popover: {
-					DEFAULT: "hsl(var(--popover) / <alpha-value>)",
-					foreground: "hsl(var(--popover-foreground) / <alpha-value>)"
-				},
-				card: {
-					DEFAULT: "hsl(var(--card) / <alpha-value>)",
-					foreground: "hsl(var(--card-foreground) / <alpha-value>)"
-				}
 			},
-			borderRadius: {
-				lg: "var(--radius)",
-				md: "calc(var(--radius) - 2px)",
-				sm: "calc(var(--radius) - 4px)"
-			},
-			fontFamily: {
-				sans: [...fontFamily.sans]
-			}
-		}
+		},
+
 	},
+	plugins: [
+		themer({
+			radius: "smoothest",
+			background: "lighter",
+			border: "light",
+			padding:"large",
+			components: {
+				button: {
+					rounded : "2xl"
+				}
+			}
+		})
+	],
 };
 
 export default config;
