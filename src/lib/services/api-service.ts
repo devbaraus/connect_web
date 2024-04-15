f60fff56c7676f6f8c477554b41a12a6576bd900
Bruno de Araújo Alves <b.araujo@kingspanisoeste.com.br>
2024-04-15 16:28:18 -0300

    refactor: ♻️ update route

--- src/lib/services/api-service.ts
+++ src/lib/services/api-service.ts
@@ -3,9 +3,9 @@ import { PUBLIC_WEB_URL } from '$env/static/public';
 
 export const ApiService = {
     busca: async (query: string, config?: RequestInit) => {
-        const url = new URL(`${PUBLIC_WEB_URL}/v1/busca`);
+        const url = new URL(`${PUBLIC_WEB_URL}/v1/search`);
         url.searchParams.set('query', query);
-        
+
         const response = await fetch(url.toString(), {
             signal: config?.signal
         });
